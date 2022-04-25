2ae803db6fa150c3371efcbce4987d75f4df6ee1
lala <laurawilther@gmail.com>
2022-04-25 22:50:27 +0700

    landing page 60% :D

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -1,5 +1,18 @@
 @extends('template.main')
 
 @section('content')
-    
+    <div class="hero container">
+        <div class=" row d-flex justify-content-center align-items-center">
+            <div class="col-6">
+                <h1>One stop solution
+                <br> for your mental health</h1>
+                <p>Ease provides a convinient and practical
+                services for your mental health problems</p>
+                <a href=""></a>
+            </div>
+            <div class="col-6">
+                <img src="/images/illustration.png" width=500 alt="">
+            </div>
+        </div>
+    </div>
 @endsection
